803207da86ba9f2ef1ff73dfa913421250ee54fb
Nate Jones <nate@app-sauce.com>
2019-02-19 13:26:45 -0800

    Support multiple documents in the stream

--- src/app/main.clj
+++ src/app/main.clj
@@ -1,11 +1,21 @@
 (ns app.main
   (:require
     [clojure.edn :as edn]
+    [clojure.java.io :as io]
     [cheshire.core :as json])
+  (:import
+    [java.io PushbackReader])
   (:gen-class))
+
+(defn edn-seq
+  [handle]
+  (let [reader (PushbackReader. (io/reader handle))]
+    (take-while
+      some?
+      (repeatedly #(if-let [obj (edn/read {:eof nil} reader)]
+                    obj
+                    (.close reader))))))
 
 (defn -main
   []
-  (-> (edn/read *in*)
-      json/encode
-      println))
+  (run! (comp println json/encode) (edn-seq *in*)))
